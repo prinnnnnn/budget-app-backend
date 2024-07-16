54a1a601ae97e6713270735d83b4ee7d9a6b5a5b
prinnnnnn <106861903+prinnnnnn@users.noreply.github.com>
2024-07-16 15:52:18 +0700

    add expense router

--- src/routes/expenseRoutes.ts
+++ src/routes/expenseRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { getAllExpenses, getExpensesByBudget } from "../controllers/expenseController";
 
 const router = Router();
 
-router.get("/", );
+router.get("/", getAllExpenses);
+router.get("/:budgetId", getExpensesByBudget)
 
 export default router;
